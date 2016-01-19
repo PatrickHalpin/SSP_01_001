cd90d218820dd2c643a303e9f7e7a1d29c84367f
PatrickHalpin <patrickhalpin@patricks-MacBook-Air.local>
2016-01-19 15:16:17 +0000

    added variables in Js

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 //just printing to the console
 console.log("hello world");
+
+function add(a,b)
+{
+	return a + b;
+}
+
+var ans=add(5,10);
+console.log(ans);
+
+var name ="pat";
+var age= 21;
+var male=true;
+var myArray = new Array();
+
+if(age==21)
+{
+	console.log("youre still young " + name)
+}
+
+var o = new Object();
+
+var anotherObject ={};
+
