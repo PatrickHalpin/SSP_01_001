c14a777a0a0110842fd6dabc8796fdab88de36ef
PatrickHalpin <patrickhalpin@patricks-MacBook-Air.local>
2016-01-19 15:37:54 +0000

    added new functions

--- app.js
+++ app.js
@@ -23,3 +23,81 @@ var o = new Object();
 
 var anotherObject ={};
 
+
+/*
+ActionScript code to create a class
+public class Person
+{
+	public var name:String;
+	public var age: int;
+	public var male: boolean;
+
+	function Person()
+	{
+		this.male=false;
+	}
+
+	public growOld()
+	{
+		this.age++;
+	}
+}
+
+var p:Person = new Person();
+p.name="pat";
+p.age=21;
+p.male=true;
+
+p.growOld();
+
+*/
+
+//creating an object in JS
+var person = {};
+person.name="pat";
+person.age=21;
+person.male=true;
+
+person.growOld=function()
+{
+	this.age++;
+};
+
+//second way - both do the same thing
+var anotherPerson=
+{
+	name: "mary",
+	age: 23,
+	male: false,
+	growOld: function()
+	{
+		this.age++;
+	},
+	friends: ["tom","pat"]
+};
+
+for(var i=0;i<10;i++)
+{
+	console.log(anotherPerson.name + " is " + anotherPerson.age);
+	anotherPerson.growOld();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
